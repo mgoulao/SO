5457e86dbf518bd0963ca45a2b9b78075fbd918a
Manuel Goulão <msilvagoulao@gmail.com>
2018-09-30 19:20:35 +0100

    start project

--- Proj1/CircuitRouter-SeqSolver/CircuitRouter-SeqSolver.c
+++ Proj1/CircuitRouter-SeqSolver/CircuitRouter-SeqSolver.c
@@ -50,7 +50,6 @@
  * =============================================================================
  */
 
-
 #include <assert.h>
 #include <getopt.h>
 #include <stdio.h>
@@ -60,31 +59,34 @@
 #include "router.h"
 #include "lib/timer.h"
 #include "lib/types.h"
+#include "lib/commandlinereader.h"
 
-enum param_types {
+enum param_types
+{
     PARAM_BENDCOST = (unsigned char)'b',
-    PARAM_XCOST    = (unsigned char)'x',
-    PARAM_YCOST    = (unsigned char)'y',
-    PARAM_ZCOST    = (unsigned char)'z',
+    PARAM_XCOST = (unsigned char)'x',
+    PARAM_YCOST = (unsigned char)'y',
+    PARAM_ZCOST = (unsigned char)'z',
 };
 
-enum param_defaults {
+enum param_defaults
+{
     PARAM_DEFAULT_BENDCOST = 1,
-    PARAM_DEFAULT_XCOST    = 1,
-    PARAM_DEFAULT_YCOST    = 1,
-    PARAM_DEFAULT_ZCOST    = 2,
+    PARAM_DEFAULT_XCOST = 1,
+    PARAM_DEFAULT_YCOST = 1,
+    PARAM_DEFAULT_ZCOST = 2,
 };
 
 bool_t global_doPrint = FALSE;
-char* global_inputFile = NULL;
+char *global_inputFile = NULL;
 long global_params[256]; /* 256 = ascii limit */
-
 
 /* =============================================================================
  * displayUsage
  * =============================================================================
  */
-static void displayUsage (const char* appName){
+static void displayUsage(const char *appName)
+{
     printf("Usage: %s [options]\n", appName);
     puts("\nOptions:                            (defaults)\n");
     printf("    b <INT>    [b]end cost          (%i)\n", PARAM_DEFAULT_BENDCOST);
@@ -96,24 +98,24 @@ static void displayUsage (const char* appName){
     exit(1);
 }
 
-
 /* =============================================================================
  * setDefaultParams
  * =============================================================================
  */
-static void setDefaultParams (){
+static void setDefaultParams()
+{
     global_params[PARAM_BENDCOST] = PARAM_DEFAULT_BENDCOST;
-    global_params[PARAM_XCOST]    = PARAM_DEFAULT_XCOST;
-    global_params[PARAM_YCOST]    = PARAM_DEFAULT_YCOST;
-    global_params[PARAM_ZCOST]    = PARAM_DEFAULT_ZCOST;
+    global_params[PARAM_XCOST] = PARAM_DEFAULT_XCOST;
+    global_params[PARAM_YCOST] = PARAM_DEFAULT_YCOST;
+    global_params[PARAM_ZCOST] = PARAM_DEFAULT_ZCOST;
 }
-
 
 /* =============================================================================
  * parseArgs
  * =============================================================================
  */
-static void parseArgs (long argc, char* const argv[]){
+static void parseArgs(long argc, char *const argv[])
+{
     long i;
     long opt;
 
@@ -121,55 +123,59 @@ static void parseArgs (long argc, char* const argv[]){
 
     setDefaultParams();
 
-    while ((opt = getopt(argc, argv, "hb:px:y:z:")) != -1) {
-        switch (opt) {
-            case 'b':
-            case 'x':
-            case 'y':
-            case 'z':
-                global_params[(unsigned char)opt] = atol(optarg);
-                break;
-            case 'p':
-                global_doPrint = TRUE;
-                break;
-            case '?':
-            case 'h':
-            default:
-                opterr++;
-                break;
+    while ((opt = getopt(argc, argv, "hb:px:y:z:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'b':
+        case 'x':
+        case 'y':
+        case 'z':
+            global_params[(unsigned char)opt] = atol(optarg);
+            break;
+        case 'p':
+            global_doPrint = TRUE;
+            break;
+        case '?':
+        case 'h':
+        default:
+            opterr++;
+            break;
         }
     }
 
-    for (i = optind; i < argc; i++) {
+    for (i = optind; i < argc; i++)
+    {
         fprintf(stderr, "Non-option argument: %s\n", argv[i]);
         opterr++;
     }
 
-    if (opterr) {
+    if (opterr)
+    {
         displayUsage(argv[0]);
     }
 }
-
 
 /* =============================================================================
  * main
  * =============================================================================
  */
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     /*
      * Initialization
      */
-    parseArgs(argc, (char** const)argv);
-    maze_t* mazePtr = maze_alloc();
+    parseArgs(argc, (char **const)argv);
+    maze_t *mazePtr = maze_alloc();
     assert(mazePtr);
 
     long numPathToRoute = maze_read(mazePtr);
-    router_t* routerPtr = router_alloc(global_params[PARAM_XCOST],
+    router_t *routerPtr = router_alloc(global_params[PARAM_XCOST],
                                        global_params[PARAM_YCOST],
                                        global_params[PARAM_ZCOST],
                                        global_params[PARAM_BENDCOST]);
     assert(routerPtr);
-    list_t* pathVectorListPtr = list_alloc(NULL);
+    list_t *pathVectorListPtr = list_alloc(NULL);
     assert(pathVectorListPtr);
 
     router_solve_arg_t routerArg = {routerPtr, mazePtr, pathVectorListPtr};
@@ -184,13 +190,13 @@ int main(int argc, char** argv){
     long numPathRouted = 0;
     list_iter_t it;
     list_iter_reset(&it, pathVectorListPtr);
-    while (list_iter_hasNext(&it, pathVectorListPtr)) {
-        vector_t* pathVectorPtr = (vector_t*)list_iter_next(&it, pathVectorListPtr);
+    while (list_iter_hasNext(&it, pathVectorListPtr))
+    {
+        vector_t *pathVectorPtr = (vector_t *)list_iter_next(&it, pathVectorListPtr);
         numPathRouted += vector_getSize(pathVectorPtr);
-	}
+    }
     printf("Paths routed    = %li\n", numPathRouted);
     printf("Elapsed time    = %f seconds\n", TIMER_DIFF_SECONDS(startTime, stopTime));
-
 
     /*
      * Check solution and clean up
@@ -204,10 +210,12 @@ int main(int argc, char** argv){
     router_free(routerPtr);
 
     list_iter_reset(&it, pathVectorListPtr);
-    while (list_iter_hasNext(&it, pathVectorListPtr)) {
-        vector_t* pathVectorPtr = (vector_t*)list_iter_next(&it, pathVectorListPtr);
-        vector_t* v;
-        while((v = vector_popBack(pathVectorPtr))) {
+    while (list_iter_hasNext(&it, pathVectorListPtr))
+    {
+        vector_t *pathVectorPtr = (vector_t *)list_iter_next(&it, pathVectorListPtr);
+        vector_t *v;
+        while ((v = vector_popBack(pathVectorPtr)))
+        {
             // v stores pointers to longs stored elsewhere; no need to free them here
             vector_free(v);
         }
@@ -215,10 +223,8 @@ int main(int argc, char** argv){
     }
     list_free(pathVectorListPtr);
 
-
     exit(0);
 }
-
 
 /* =============================================================================
  *
---
+++ Proj1/CircuitRouter-SeqSolver/tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Test 1
+echo Make - $(make)
+echo Run
+echo $(./CircuitRouter-SeqSolver p p < inputs/random-x32-y32-z3-n64.txt) 
