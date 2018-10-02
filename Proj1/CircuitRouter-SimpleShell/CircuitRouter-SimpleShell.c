f52d2051f0cfeff8f7518b133a711ae51c76d01d
Manuel Goulão <msilvagoulao@gmail.com>
2018-10-02 11:29:36 +0100

    Add run command

--- Proj1/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
+++ Proj1/CircuitRouter-SimpleShell/CircuitRouter-SimpleShell.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "lib/types.h"
 #include "lib/commandlinereader.h"
 
 #define BUFFER_SIZE 80
 #define NUM_INPUTS 2
+
+void runCommand(char *inputFile)
+{
+    int pid;
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Unable to fork\n");
+    }
+    else if (pid == 0)
+    {
+        printf("-----filho\n");
+        if (inputFile != NULL)
+        {
+            execl("../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver", inputFile, (char *)NULL);
+        }
+        else
+        {
+            printf("Input file needed\n");
+            exit(1);
+        }
+    }
+    /* else
+    {
+        wait(&state);
+    }  */
+}
+
+void exitCommand()
+{
+    printf("----exit\n");
+}
 
 int main(int argc, char *argv[])
 {
@@ -16,26 +53,20 @@ int main(int argc, char *argv[])
     while (TRUE)
     {
         numArgs = readLineArguments(argsVector, NUM_INPUTS + 1, buffer, BUFFER_SIZE);
-        printf("%d", strcmp("run", argsVector[0]));
-        if (strcmp("run", argsVector[0]))
+        if (!strcmp("run", argsVector[0]))
         {
-            printf("RUN FOREST RUN!!!!");
+            runCommand(argsVector[1]);
         }
-        else if (strcmp("exit", argsVector[0]))
+        else if (!strcmp("exit", argsVector[0]))
         {
-            printf("Goodbye");
+            exitCommand();
             break;
         }
         else
         {
-            printf("Command is stupid");
+            printf("Invalid command\n");
         }
     }
-    printf("numArgs - %d, arg1: %s, arg2: %s", numArgs, argsVector[0], argsVector[1]);
 
     return 0;
-}
-
-void prompt()
-{
 }
